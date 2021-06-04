46f8b11cc79acf38914334655e4c96ccdbcc8337
Kirijo <asyelsub@gmail.com>
2021-06-04 11:27:21 -0400

    Outline for OOP classes, not coded yet.

---
+++ Assets/Scripts/Class_Addition/P_Addition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Addition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
