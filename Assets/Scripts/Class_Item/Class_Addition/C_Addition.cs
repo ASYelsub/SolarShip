c74d7478dd40fd84e4c737a557e8091a71d5ada9
Abigail <asyelsub@gmail.com>
2021-06-08 10:17:05 -0400

    a terrible day/night cycle... need to make it make sense in terms of a circular cycle instead of whatever is currently going on.

--- Assets/Scripts/Class_Item/Class_Addition/C_Addition.cs
+++ Assets/Scripts/Class_Item/Class_Addition/C_Addition.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class C_Addition : P_Item
 {
+    //the six corners of the room
+    Vector3[] corners;
+
+    //the doors that are connected to the room
+    List<C_Connector> doors;
+
+
     public C_Addition(ItemState itemState, int itemTypeID, int itemID) : base(itemState, itemTypeID, itemID)
     {
     }
---
+++ Assets/Scripts/DayNightManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNightManager : MonoBehaviour
+{
+    public Text dayNightLabelText;
+    public Transform directionalLightTransform;
+
+
+    //probably .01f
+    public float dayChangeSpeed;
+    float timeOfDay = 0f;
+
+    
+
+    private void FixedUpdate()
+    {
+        if(timeOfDay >= 24)
+        {
+            timeOfDay = 0;
+        }
+        else
+        {
+            timeOfDay += dayChangeSpeed;
+            directionalLightTransform.transform.Rotate(dayChangeSpeed, 0, 0);
+            dayNightLabelText.text = "Time of Day" + timeOfDay.ToString();
+        }
+    }
+}
