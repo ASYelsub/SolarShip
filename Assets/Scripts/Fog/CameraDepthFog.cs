0c9fcd4048e3e47ce282b2e11a16ce6f1f729da8
Abigail <asyelsub@gmail.com>
2021-06-24 12:17:34 -0400

    edited ship a bit

---
+++ Assets/Scripts/Fog/CameraDepthFog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class CameraDepthFog : MonoBehaviour
+{
+    public Material fogMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // allows implementation of fog post process
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, fogMat);
+    }
+}
